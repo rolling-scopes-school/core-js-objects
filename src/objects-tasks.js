db2f8879fa652a817bd6177235922f74eb2b0593
Maxim <104011221+maximozaitsev@users.noreply.github.com>
2024-03-05 19:44:40 +0300

    fix: warning with unnamed function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -206,10 +206,11 @@ function sellTickets(queue) {
 function Rectangle(width, height) {
   this.width = width;
   this.height = height;
-  this.getArea = function () {
-    return this.width * this.height;
-  };
 }
+
+Rectangle.prototype.getArea = function getArea() {
+  return this.width * this.height;
+};
 
 /**
  * Returns the JSON representation of specified object
