8c096f08c4efcf0c4a86a081e318ead50d8b83af
alinalitvinka <alinalitvinko7@gmail.com>
2025-03-09 17:34:07 +0300

    feat: solve isEmptyObject

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -91,8 +91,8 @@ function compareObjects(obj1, obj2) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
 }
 
 /**
