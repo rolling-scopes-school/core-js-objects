ace303c8693ded079c13a4e1c032618a6a756bb0
alinalitvinka <alinalitvinko7@gmail.com>
2025-03-09 11:07:49 +0300

    feat: solve mergeObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,14 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const result = objects.reduce((accumulator, currentValue) => {
+    Object.entries(currentValue).forEach(([key, value]) => {
+      accumulator[key] = (accumulator[key] || 0) + value;
+    });
+    return accumulator;
+  }, {});
+  return result;
 }
 
 /**
