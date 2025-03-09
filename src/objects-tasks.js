8af93142070685bed03b591ff84f4a345f6dad45
alinalitvinka <alinalitvinko7@gmail.com>
2025-03-09 17:17:31 +0300

    feat: solve removeProperties

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -56,8 +56,12 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const result = obj;
+  keys.forEach((key) => {
+    delete result[key];
+  });
+  return result;
 }
 
 /**
