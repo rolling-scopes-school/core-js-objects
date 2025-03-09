996b93c030d2811573c586898c018be17b5ca285
alinalitvinka <alinalitvinko7@gmail.com>
2025-03-09 17:27:42 +0300

    feat: solve compareObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -76,8 +76,8 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
 /**
